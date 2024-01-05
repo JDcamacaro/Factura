ba6556dbdf44ffc5beaedd32844a17c121f02b8e
Jesus Camacaro <143712820+JDcamacaro@users.noreply.github.com>
2024-01-05 19:13:28 -0400

    Add files via upload

---
+++ projet_david/src/main/java/com/bppr/projetDavid/ProjetDavidApplication.java
@@ -0,0 +1,13 @@
+package com.bppr.projetDavid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDavidApplication.class, args);
+	}
+
+}
---
+++ projet_david/src/test/java/com/bppr/projetDavid/ProjetDavidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bppr.projetDavid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDavidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
